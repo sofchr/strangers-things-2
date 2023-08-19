4163def06fd348757a1bed21c9c5c4966761c47b
Sofie Christensen <sofiec510@gmail.com>
2023-08-18 23:24:19 -0400

    testing setup and first test

--- src/App.test.js
+++ src/App.test.js
@@ -5,23 +5,24 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 import "@testing-library/jest-dom";
+import AllPosts from "./components/AllPosts"
 
 describe("App component", () => {
-  test("displays the details of a featured puppy when clicked", () => {
+  test("renders all posts", () => {
     // Render the App component
-    render(<App />);
+    render(<AllPosts />);
 
-    // Find the puppy with name "Sir Waggington"
-    const puppyName = screen.getByText("Sir Waggington");
+    // Find the nav bar button for all posts
+    const allPostsButton = screen.getByText("All Posts");
 
-    // Simulate a click on the puppy
-    fireEvent.click(puppyName);
+    // Simulate a click on the "All Posts" button
+    fireEvent.click(allPostsButton);
 
-    // Assert that the featured puppy's name is displayed
-    const featuredPuppyName = screen.getByRole("heading", {
-      name: /Sir Waggington/i,
-    });
-    expect(featuredPuppyName).toBeInTheDocument();
+    // Assert that the header above all posts is rendered
+    const headerText = screen.getByText(
+      /All Active Posts Below/i,
+    );
+    expect(headerText).toBeInTheDocument();
 
     // Assert that the featured puppy's age is displayed
     const featuredPuppyAge = screen.getByText(/Age: \d+/);
@@ -32,31 +33,6 @@ describe("App component", () => {
     expect(featuredPuppyEmail).toBeInTheDocument();
   });
 
-  // TODO
-  test("does not display the details of a featured puppy initially", () => {
-    // Render the App component
-    // Assert that the featured puppy's name is not displayed
-    // Assert that the featured puppy's age is not displayed
-    // Assert that the featured puppy's email is not displayed
-  });
 
-  // TODO
-  test("does not change the featured puppy when the same puppy is clicked twice", () => {
-    // Render the App component
-    // Find the puppy with name "Sir Waggington"
-    // Simulate a click on the puppy
-    // Assert that the initial featured puppy's name is displayed
-    // Simulate another click on the same puppy
-    // Assert that the featured puppy's name is still displayed and is the same as the initial featured puppy
-  });
-
-  // TODO
-  test("displays the details of a featured puppy when clicked", () => {
-    // Render the App component
-    // Find the puppy with name "Miss Furbulous"
-    // Simulate a click on the puppy
-    // Assert that the featured puppy's name is displayed
-    // Assert that the featured puppy's age is displayed
-    // Assert that the featured puppy's email is displayed
-  });
+  //test below for API call
 });
